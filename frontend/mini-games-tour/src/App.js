4583fe962a58fb335363fab13cbd6493426d236c
Robertas <robervicsat@gmail.com>
2025-09-10 20:04:22 +0300

    Added Routing to react, created pages folder with home and game's pages

--- frontend/mini-games-tour/src/App.js
+++ frontend/mini-games-tour/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import RockPaperScissorsGame from './pages/RockPaperScissorsGame/RockPaperScissorsGame';
+import FourInARowGame from './pages/FourInARowGame/FourInARowGame';
+import PairMatchingGame from './pages/PairMathingGame/PairMatchingGame';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rock-paper-scissors" element={<RockPaperScissorsGame />} />
+        <Route path="/four-in-a-row" element={<FourInARowGame />} />
+        <Route path="/pair-matching" element={<PairMatchingGame />} />
+      </Routes>
+    </Router>
   );
 }
 
