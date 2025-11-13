1d270a0bbbf776c797cfcf7d99f348d38293b198
Robertas <robervicsat@gmail.com>
2025-11-13 22:56:56 +0200

    wrote some tests with registration and jwt token

--- backend/Tests/UserServiceTests.cs
+++ backend/Tests/UserServiceTests.cs
@@ -4,6 +4,10 @@ using Microsoft.EntityFrameworkCore;
 using Models;
 using Data;
 using Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace backend.Tests
 {
@@ -22,17 +26,24 @@ namespace backend.Tests
             _service = new UserService(_context);
         }
 
-        [Fact]
-        public async Task GetUserAsync_Should_Return_User_When_Found()
+        [Theory]
+        [InlineData("robert", "hash123")]
+        [InlineData("alice", "hash456")]
+        [InlineData("bob", "hash789")]
+        public async Task GetUserAsync_Should_Return_User_When_Found(string username, string passwordHash)
         {
-            var user = new User { Username = "robert", PasswordHash = "hash123" };
+            // Arrange
+            var user = new User { Username = username, PasswordHash = passwordHash };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
-            var result = await _service.GetUserAsync("robert");
+            // Act
+            var result = await _service.GetUserAsync(username);
 
+            // Assert
             result.Should().NotBeNull();
-            result!.Username.Should().Be("robert");
+            result!.Username.Should().Be(username);
+            result.PasswordHash.Should().Be(passwordHash);
         }
 
         [Fact]
@@ -60,5 +71,100 @@ namespace backend.Tests
             var user = await _context.Users.FindAsync("newuser");
             user.Should().NotBeNull();
         }
+
+        [Fact]
+        public async Task RegisterUserAsync_Should_Return_False_When_User_Already_Exists()
+        {
+            var existingUser = new User
+            {
+                Username = "existinguser",
+                PasswordHash = "hash123"
+            };
+            _context.Users.Add(existingUser);
+            await _context.SaveChangesAsync();
+
+            var result = await _service.RegisterUserAsync("existinguser", "newpassword");
+
+            result.Should().BeFalse();
+
+            var usersWithSameUsername = await _context.Users.CountAsync(u => u.Username == "existinguser");
+            usersWithSameUsername.Should().Be(1);
+        }
+
+        [Fact]
+        public void GenerateJwtToken_Should_Return_Valid_Jwt_For_User()
+        {
+            var user = new User { Username = "robert", PasswordHash = "hash123" };
+
+            var tokenString = _service.GenerateJwtToken(user);
+
+            tokenString.Should().NotBeNullOrEmpty();
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(tokenString);
+
+            var nameClaim = token.Claims.FirstOrDefault(c => c.Type == "unique_name");
+            nameClaim.Should().NotBeNull();
+            nameClaim!.Value.Should().Be("robert");
+
+            token.ValidTo.Should().BeAfter(DateTime.UtcNow);
+        }
+
+        [Fact]
+        public async Task GetUserFromTokenAsync_Should_Return_User_When_Token_Is_Valid()
+        {
+            var username = "robert";
+            var passwordHash = "hash123";
+            var user = new User { Username = username, PasswordHash = passwordHash };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            var token = _service.GenerateJwtToken(user);
+
+            var result = await _service.GetUserFromTokenAsync(token);
+
+            result.Should().NotBeNull();
+            result!.Username.Should().Be(username);
+            result.PasswordHash.Should().Be(passwordHash);
+        }
+
+        [Fact]
+        public async Task GetUserFromTokenAsync_Should_Return_Null_When_Token_Has_Invalid_Signature()
+        {
+            var fakeKey = Encoding.ASCII.GetBytes("this-is-a-very-long-fake-secret-key-123!");
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "robert") }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(fakeKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+
+            var result = await _service.GetUserFromTokenAsync(jwtToken);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetUserFromTokenAsync_Should_Return_Null_When_Token_Has_No_Username_Claim()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(UserService.KEY), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+
+            var result = await _service.GetUserFromTokenAsync(jwtToken);
+
+            result.Should().BeNull();
+        }
+
+
     }
 }
